e3fe754f226caf5a6fad6a266fa37f0d6aee6415
Chuck Logan Lim <chucky_da_great@yahoo.com>
2016-01-15 17:55:00 +0800

    test_home_page_returns_correct_html now working properly

--- demo/tests.py
+++ demo/tests.py
@@ -1,25 +1,31 @@
-from django.core.urlresolvers import resolve, reverse
-from django.test import TestCase, RequestFactory
+from django.core.urlresolvers import reverse
+from django.test import TestCase, RequestFactory, Client
 
 from .factories import UserFactory
-from .models import AffiliatedSchool
-from .views import IndexView
 
 
 class HomePageTest(TestCase):
 
     """
-    Test for home page
+    Tests for home page
     """
+
+    # TODO: test the following
+    # GET or POST by anonymous user (should redirect to login page)
+	# GET or POST by logged-in user with no profile (should raise a UserProfile.DoesNotExist exception)
+	# GET by logged-in user (should show the form)
+	# POST by logged-in user with blank data (should show form errors)
+	# POST by logged-in user with invalid data (should show form errors)
+	# POST by logged-in user with valid data (should redirect)
+	# http://stackoverflow.com/questions/11885211/how-to-write-a-unit-test-for-a-django-view
 
     def setUp(self):
         self.user = UserFactory()
         self.factory = RequestFactory()
+        self.client = Client()
 
     def test_home_page_returns_correct_html(self):
-        request = self.factory.get(reverse('index'))
-        request.user = self.user
-        response = IndexView.as_view()(request)
-        response.render()
+        url = reverse('index')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'<title>PSYSC Database</title>', response.content)
+        self.assertTemplateUsed(response, 'index.html')
